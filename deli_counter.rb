370548c6cb3606edbeeb1491c6d7b793724d512b
dzeit216@gmail.com Zeit <dzeit216@gmail.com>
2018-03-28 19:35:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,9 +13,6 @@ end
 
 def take_a_number(current_line, name)
   current_line.push(name)
-  
- puts "Welcome, #{name}. You are number #{} in line."
-  
-
+  puts "Welcome, #{name}. You are number #{current_line.length} in line."
 end 
   

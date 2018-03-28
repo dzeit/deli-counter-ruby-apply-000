19b30bcbd9e7724628e1caf9a793578f8347bc00
dzeit216@gmail.com Zeit <dzeit216@gmail.com>
2018-03-28 18:33:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(names)
   if names.length == 0 
     puts "The line is currently empty."
   else 
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     names.each.with_index(1) do |name, i|
-      current_line << "#{i}. #{name} "
+      current_line << " #{i}. #{name}"
     end 
     puts current_line
   end

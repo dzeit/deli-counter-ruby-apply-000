6632e99ef9cba1bca20cea06a8dee2329a2b8684
dzeit216@gmail.com Zeit <dzeit216@gmail.com>
2018-03-28 17:54:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,13 @@
 # Write your code here 
 def line(names)
-  
-names.each do |name|
-  puts "#{x}. #{name}"
-end
+  if line < 1 
+    puts "The line is currently empty."
+  else 
+    
+  end 
+  names.each do |name|
+    puts "#{x}. #{name}"
+  end
 
 end 
 
